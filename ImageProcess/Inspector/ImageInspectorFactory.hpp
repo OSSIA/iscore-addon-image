628c6e599cf40f4e92d6c98aa0e83eaa8940fef9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-18 14:43:13 +0200

    simplify

--- ImageProcess/Inspector/ImageInspectorFactory.hpp
+++ ImageProcess/Inspector/ImageInspectorFactory.hpp
@@ -1,29 +1,13 @@
 #pragma once
 #include <Process/Inspector/ProcessInspectorWidgetDelegateFactory.hpp>
-
-class InspectorWidgetBase;
-class QObject;
-class QString;
-class QWidget;
-namespace iscore {
-struct DocumentContext;
-}  // namespace iscore
-
+#include <ImageProcess/ImageModel.hpp>
+#include <ImageProcess/Inspector/ImageInspectorWidget.hpp>
 
 namespace Image
 {
-class InspectorFactory final : public Process::InspectorWidgetDelegateFactory
+class InspectorFactory final :
+        public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
 {
         ISCORE_CONCRETE_FACTORY("6c6a91ad-8267-4976-a15d-de1cfce434e6")
-    public:
-        InspectorFactory() = default;
-
-    private:
-        Process::InspectorWidgetDelegate* make(
-                const Process::ProcessModel&,
-                const iscore::DocumentContext& doc,
-                QWidget* parent) const override;
-        bool matches(const Process::ProcessModel&) const override;
-
 };
 }
